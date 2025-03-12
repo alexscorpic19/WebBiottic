fdf646ce39b102d717cefda3bbfb6205c7d9a026
alexscorpic19 <alexscorpic19@gmail.com>
2025-03-12 18:39:03 -0500

    tratando de arreglas el deplegue

--- src/server/scripts/test-email.ts
+++ src/server/scripts/test-email.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
+const __filename = require.main?.filename || '';
 const __dirname = path.dirname(__filename);
 
 // Cargar variables de entorno desde el directorio raíz
