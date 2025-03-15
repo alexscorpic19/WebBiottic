a3495f139f713cf3477b0c150630bfe943107716
alexscorpic19 <alexscorpic19@gmail.com>
2025-03-14 22:14:01 -0500

    se modifico el archivo ecosystem, para tratar de corregir el error de despliegue

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -22,11 +22,12 @@ module.exports = {
     wait_ready: true,
     kill_timeout: 5000,
     listen_timeout: 10000,
-    // Modificar el script post_update para asegurar que todas las dependencias se instalen
+    // Modificar el script post_update para asegurar que todas las dependencias se instalen y se ejecute el build
     post_update: [
       "cd /home/admin/web/test.biottic.com.co/public_html",
       "pnpm install",
       "pnpm run build",
+      "pnpm install --prod",
       "echo 'Verificando dependencias instaladas:'",
       "ls -la node_modules/cors node_modules/joi"
     ]
