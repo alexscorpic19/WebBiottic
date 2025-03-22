8754bc575a0dddb6976cee6a94713df60231b293
alexscorpic19 <alexscorpic19@gmail.com>
2025-03-22 14:51:13 -0500

    modificamos todos los archivos que usan la ruta para encontrar el archiv .env

--- src/server/scripts/run-tests.ts
+++ src/server/scripts/run-tests.ts
@@ -1,12 +1,9 @@
 import dotenv from 'dotenv';
 import path from 'path';
-import { fileURLToPath } from 'url';
 import nodemailer from 'nodemailer';
 import mongoose from 'mongoose';
 
 // Configurar variables de entorno
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.resolve(process.cwd(), '.env') });
 
 const runTests = async () => {
