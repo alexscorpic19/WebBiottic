8ad3877c16c45e677d43bdf22c512a0c651a3469
alexscorpic19 <alexscorpic19@gmail.com>
2025-03-25 12:54:25 -0500

    se modifico el ecosystem, buscando que la comunicacion funcione entre el back y el front

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,14 +1,11 @@
 module.exports = {
   apps: [{
-    name: 'biottic-server',
-    script: 'server/index.js',
-    instances: 4,
-    exec_mode: 'cluster',
-    env: {
-      NODE_ENV: 'development'
-    },
+    name: "biottic-server",
+    script: "./server/index.js",
+    instances: "max",
+    exec_mode: "cluster",
     env_production: {
-      NODE_ENV: 'production',
+      NODE_ENV: "production",
       PORT: 3000
     },
     wait_ready: true,
@@ -16,6 +13,7 @@ module.exports = {
     error_file: '/var/log/biottic/err.log',
     out_file: '/var/log/biottic/out.log',
     merge_logs: true,
-    time: true
+    time: true,
+    cwd: "/home/admin/web/test.biottic.com.co/public_html"
   }]
 };
